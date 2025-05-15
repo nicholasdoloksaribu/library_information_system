942576044688330e17910563cfe4e4596d08eb88
Nicholas Doloksaribu <nicholasdoloksaribu450@gmail.com>
2025-05-15 14:57:13 +0700

    mengetest git pull

--- library_information_system(backend)/database/seeders/BorrowingSeeder.php
+++ library_information_system(backend)/database/seeders/BorrowingSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Borrowing;
+use Carbon\Carbon;
+use Faker\Factory as Faker; 
 use App\Models\Book; // Import the Book model
 use App\Models\Borrowing; // Import the Borrowing model
 use App\Models\Student; // Import the Student model
